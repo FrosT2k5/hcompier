8b49226f3d2059a37d1e06a566ce8a778d885352
FrosT2k5 <62540315+FrosT2k5@users.noreply.github.com>
2020-05-13 14:16:14 +0530

    Add files via upload

---
+++ hcompiler.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+def clr():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+def help():
+    print('Currently only print command is available, the syntax is: print(__<text u want to print>__) \n Example: print(__Hello world__)') 
+
+clr()
+def main():
+    minp = str(input('HCOMPILER: '))
+    num = len(minp)
+    numa = num - 3
+    inp1 = minp[0:5]
+
+    if inp1 == 'print': 
+        pinp = minp[8:numa]
+        clr()
+        print(pinp)
+        main()
+    elif inp1 == 'help':
+        help()
+        main()
+    elif inp1 == 'clear' or inp1 == 'clr':
+        clr()
+        main()
+    elif inp1 == 'exit':
+        sys.exit()
+    else:
+        print('Wrong input')
+        help()
+        main()
+main()
